1136ebfa2cade13f266810190decf5bb60668065
jek10 <jekalviar10@gmail.com>
2016-05-20 16:52:40 +0800

    Some adjustment admin button added.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
-    posts = Post.objects.order_by('-published_date')
+    posts = Post.objects.order_by('-created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-
-
